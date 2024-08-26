819d6ab383af74374e802d3d147f1bc623c3c7af
John Rodriguez <johnmaverickox@gmail.com>
2024-08-26 17:55:20 -0400

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
-
-
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import DashBoard from './pages/DashBoard'
+import Projects from './pages/Projects'
 function App() {
 
-
-  return (
+return (
     <>
-     <p className='text-3xl'>Testing</p>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/dashboard' element={<DashBoard/>} />
+      <Route path='/projects' element={<Projects/>} />
+      
+    </Routes>
+  </BrowserRouter>
     </>
   )
 }
