26c60d6e6c2ce83bd37057547de121fc7afa2492
John Rodriguez <johnmaverickox@gmail.com>
2024-08-28 12:56:13 -0400

    added middle ware for errors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
+//////////////////////////////////---Base 
 import express from 'express'
 import mongoose from 'mongoose';
-//////////////////////////////////
+/////////////////////////////---Route Imports
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
-/////////////////////////////
 
+/////////////////////---
 import dotenv from 'dotenv'
 dotenv.config()
-/////////////////////
+
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log('Mongo is Connected')
@@ -21,6 +22,17 @@ app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server Connected");
 })
-
+//////////////////////////---Routes
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+
+////////////////////--MiddleWare
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message ||'Internal SeverError';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
