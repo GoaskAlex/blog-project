af5db9534484d09c37952fa6d87476cfd78c64f0
John Rodriguez <johnmaverickox@gmail.com>
2024-09-24 18:47:09 -0400

    content changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -55,7 +55,7 @@ export default function Header() {
     <>
         <Navbar className='border-b-2'>
             <Link to ='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white'>Gamers</span>Heaven
+            <span className='px-2 py-1 mr-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-red-500 rounded-lg text-white'>GoAsk.Dev</span>John
             </Link>
             
             <form className='flex relative hidden' onSubmit={handleSearchSubmit}>
