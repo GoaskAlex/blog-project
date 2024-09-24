b9b06bee928ec2e248d0fadb5cdf48dc50630dbb
John Rodriguez <johnmaverickox@gmail.com>
2024-09-24 15:57:42 -0400

    added styles to projects page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import CallToAction from './../components/CallToAction';
 export default function Projects() {
   return (
-    <div>
-      Projects
-    </div>
+    <>
+      <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+        <h1 className='text-3xl font-semibold'>Projects Coming Soon</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nesciunt aliquam saepe at architecto vero 
+          deserunt iste vitae neque blanditiis assumenda maiores modi, 
+          rem animi impedit quaerat! Maiores, laboriosam quod!</p>
+          <CallToAction/>
+      </div>
+    </>
   )
 }
