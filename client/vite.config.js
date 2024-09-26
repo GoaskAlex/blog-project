312ce596debd3b484db8ae8c5dcb61ca4ca6db06
John Rodriguez <johnmaverickox@gmail.com>
2024-09-26 15:00:44 -0400

    testing something

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':{
-      target:'http://localhost:3000',
+    //   '/api':{
+    //   target:'http://localhost:3000',
+    //   secure:false,
+    // },
+    '/api':{
+      target:'https://goaskdev-john-blog.onrender.com',
       secure:false,
     },
   },
