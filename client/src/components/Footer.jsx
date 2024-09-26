a327fe4c549a97df4efe740edab805acb6e7d767
John Rodriguez <johnmaverickox@gmail.com>
2024-09-26 15:17:03 -0400

    graphic fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,9 +13,9 @@ export default function FooterCom() {
                 to="/"
                 className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">
-                  Gamers
+                  GoAsk.Dev
                 </span>
-                Heaven
+                John
               </Link>
             </div>
             <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
