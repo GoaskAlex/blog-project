53589784c7cf3567a16ea5a5b9e28aaaadcf3e04
John Rodriguez <johnmaverickox@gmail.com>
2024-09-26 15:22:25 -0400

    minor fixes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function FooterCom() {
               <Link
                 to="/"
                 className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
-                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">
+                <span className="px-2 mr-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">
                   GoAsk.Dev
                 </span>
                 John
