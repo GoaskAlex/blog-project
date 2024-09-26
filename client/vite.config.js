2e972fe2d2b0a7ea1aa0202b27167278cb7de09f
John Rodriguez <johnmaverickox@gmail.com>
2024-09-26 15:04:45 -0400

    changed back settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy:{
-    //   '/api':{
-    //   target:'http://localhost:3000',
-    //   secure:false,
-    // },
-    '/api':{
-      target:'https://goaskdev-john-blog.onrender.com',
+      '/api':{
+      target:'http://localhost:3000',
       secure:false,
     },
   },
